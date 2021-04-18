9926f2f63212ca59e888131c7b5919635ad4c589
澄三彩 <634725355@qq.com>
2021-04-18 16:46:52 +0800

    init

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,3 +4,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export const config = {
+  api: {
+    
+  }
+}
