d15733415c5866644655e881444e405f8665af88
澄三彩 <634725355@qq.com>
2021-07-07 16:33:01 +0800

    完善scroll 下一步添加scroll组件

--- pages/demo/demo.tsx
+++ pages/demo/demo.tsx
@@ -1,11 +1,56 @@
 import Head from "next/head";
 import Link from "next/link";
+import BetterScroll from "better-scroll";
+import React, { useState, useRef, useEffect } from "react";
+
 import Layout, { siteTitle } from "../../components/Layout/layout";
+import styles from "./demo.module.css";
+
+const emojis = [
+    "👉🏼 😁 😂 🤣 👈🏼",
+    "😄 😅 😆 😉 😊",
+    "😫 😴 😌 😛 😜",
+    "👆🏻 😒 😓 😔 👇🏻",
+    "😑 😶 🙄 😏 😣",
+    "😞 😟 😤 😢 😭",
+    "🤑 😲 ☹️ 🙁 😖",
+    "👍 👎 👊 ✊ 🤛",
+    "☝️ ✋ 🤚 🖐 🖖",
+    "👍🏼 👎🏼 👊🏼 ✊🏼 🤛🏼",
+    "☝🏽 ✋🏽 🤚🏽 🖐🏽 🖖🏽",
+    "🌖 🌗 🌘 🌑 🌒",
+];
 
 export default function DemoBatterScroll() {
+    const [data, setData] = useState(emojis);
+    const [mBetter, setMBatter] = useState(null);
+    const scrollRef = useRef(null);
+
+    useEffect(() => {
+        let betterScroll = new BetterScroll(scrollRef.current, {
+            scrollX: true,
+            probeType: 3
+        });
+        console.log("初始化", betterScroll);
+
+        setMBatter(betterScroll);
+
+        return () => setMBatter(null);
+    }, []);
+
     return (
         <Layout>
-            231231
+            <div className={styles.box} ref={scrollRef} >
+                <div className={styles.content} >
+                    {
+                        data.map(p => {
+                            return (
+                                <div className={styles.contentItem} >{p}</div>
+                            );
+                        })
+                    }
+                </div>
+            </div>
         </Layout>
     );
 }
