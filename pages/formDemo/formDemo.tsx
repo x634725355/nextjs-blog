a27d2ead5b016890881b5b5c58eb2c56d3e1643c
澄三彩 <634725355@qq.com>
2021-07-22 17:18:25 +0800

    添加Layout

--- pages/formDemo/formDemo.tsx
+++ pages/formDemo/formDemo.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Form, Select } from 'antd';
 import FormExtract from "../../components/formExtract/formExtract";
+import Layout from "../../components/Layout/layout";
 
 const { Option } = Select;
 
@@ -11,26 +12,28 @@ export default function FormDemo(props: csc.Props) {
 
 
     return (
-        <div>
-            <h2>打开控制台查看效果哦</h2>
+        <Layout>
+            <div>
+                <h2>打开控制台查看效果哦</h2>
 
-            <h1>{ JSON.stringify(data) }</h1>
+                <h1>{ JSON.stringify(data) }</h1>
 
-            <Form form={form} onValuesChange={() => { setData({...form.getFieldsValue()}); console.log("让你康康哦", form.getFieldsValue()) }} >
-                <FormExtract
-                    form={form}
-                    config={[
-                        { name: 'shipCode', keyName: ['shipName'] },
-                    ]}
-                >
-                    <Form.Item name="shipCode" label="shipCode">
-                        <Select>
-                            <Option key="EA" value="TPM_SEA" shipName="SEA">SEA</Option>
-                            <Option key="AR" value="TPM_AIR" shipName="AIR">AIR</Option>
-                        </Select>
-                    </Form.Item>
-                </FormExtract>
-            </Form>
-        </div>
+                <Form form={form} onValuesChange={() => { setData({...form.getFieldsValue()}); console.log("让你康康哦", form.getFieldsValue()) }} >
+                    <FormExtract
+                        form={form}
+                        config={[
+                            { name: 'shipCode', keyName: ['shipName'] },
+                        ]}
+                    >
+                        <Form.Item name="shipCode" label="shipCode">
+                            <Select>
+                                <Option key="EA" value="TPM_SEA" shipName="SEA">SEA</Option>
+                                <Option key="AR" value="TPM_AIR" shipName="AIR">AIR</Option>
+                            </Select>
+                        </Form.Item>
+                    </FormExtract>
+                </Form>
+            </div>
+        </Layout>
     );
 }
